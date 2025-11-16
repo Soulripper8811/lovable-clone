81d0f8773144590ffe3680ddc89bcf9dfafc6b86
Soulrippper8811 <134708634+Soulripper8811@users.noreply.github.com>
2025-11-16 18:50:14 +0530

    added the inngest agentic kit

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,23 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "15s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert nextjs developer. You write readable,maintainable and scalable code. You write simple Next,js & React snippets.",
+      model: openai({
+        model: "openai/gpt-oss-20b:free",
+        baseUrl: "https://openrouter.ai/api/v1",
+        apiKey: process.env.OPENROUTER_API_KEY,
+      }),
+    });
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+    return { output };
   }
 );
