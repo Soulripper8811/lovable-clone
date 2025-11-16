077d5fbe2c40853ab8630c0b132753abb120244f
Soulrippper8811 <134708634+Soulripper8811@users.noreply.github.com>
2025-11-16 16:35:24 +0530

    added the trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import { getQueryClient, trpc } from "@/trpc/server";
+import React, { Suspense } from "react";
+import ClientTest from "./ClientTest";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-const Page = () => {
-  console.log(process.env.DATABASE_URL);
-  return <div>Hello world</div>;
+const Page = async () => {
+  const queryCient = getQueryClient();
+  void queryCient.prefetchQuery(trpc.createAi.queryOptions({ text: "amit" }));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryCient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ClientTest />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
