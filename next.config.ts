de1aa67b8c857206bd5417e5db80d765b1e290c0
Soulrippper8811 <134708634+Soulripper8811@users.noreply.github.com>
2025-11-27 14:15:37 +0530

    deployemnt chanegs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
